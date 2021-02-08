31492a88d11c607b3700eca34720412bccd6d5fa
Tamuno-omi <tamjaja@yahoo.com>
2021-02-08 05:54:55 +0000

    Initial model commit

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,13 +1,71 @@
 import sys
+import os
+import re
+
+from sqlalchemy import create_engine
+import pickle
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from scipy.stats import gmean
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, AdaBoostClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.base import BaseEstimator,TransformerMixin
+from sklearn.metrics import classification_report, accuracy_score
 
 
 def load_data(database_filepath):
-    pass
+    '''Function to load in sqlite database from file
+    Input Parameters
+    database_filepath: path to sqlite database
+    
+    Returns
+    X: Messages column(feature)
+    Y: Categories columns(dependent variable)
+    category_names: names of categories
+    '''
+    engine = create_engine('sqlite:///DisasterResponse.db')
+    df = pd.read_sql_table('Disasters', engine)
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    category_names = list(df.columns[4:])
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    """ tokenize function for processing text data
+    
+        Input parameters
+        text : str, text to tokenize
+    
+        Returns
+        clean_tokens: list, list of tokens
+        """   
+    # remove punctutation marks
+    text = re.sub(r'[^a-zA-Z0-9]',' ',text)
+    
+    # break (text) into individual linguistic units
+    tokens = word_tokenize(text)
+    
+    # lemmatize to remove inflections and derivations from a word
+    lemmatizer = WordNetLemmatizer()
 
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 def build_model():
     pass
